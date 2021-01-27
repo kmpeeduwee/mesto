e08e039e5531a62c16396a226baf4d8c133fed6d
Sergey Kartsev <kartsevsb@gmail.com>
2021-01-27 20:09:49 +0300

    add two pupup

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,15 +1,18 @@
 let body = document.querySelector('.body');
-
 let popup = body.querySelector('.popup');
-
+let popupPlace = body.querySelector('.popup_image')
 let openFormButton = body.querySelector('.profile__editButton');
+let openFormPlaceButton = body.querySelector('.profile__addButton');
 let nameInput = body.querySelector('.profile__name');
 let jobInput = body.querySelector('.profile__profession');
 let formEditProfile = body.querySelector('.popup__container');
-
+let formAddPlace = body.querySelector('.popup__container_type_image');
 let closeFormButton = formEditProfile.querySelector('.popup__closeButton');
+let closeFormPlaceButton = formAddPlace.querySelector('.popup__closeButton_type_card');
 let firstNameInput = formEditProfile.querySelector('.popup__input_type_name');
 let secondNameInput = formEditProfile.querySelector('.popup__input_type_profession');
+let placeNameInput = formAddPlace.querySelector('.popup__input_type_place');
+let placeImageInput = formAddPlace.querySelector('.popup__input_type_image');
 
 function openPopup () {
   firstNameInput.value = nameInput.textContent;
@@ -28,6 +31,19 @@ function formSubmitHandler(evt) {
   closePopup ();
 }
 
+function openPopupPlace () {
+  placeNameInput.value = placeNameInput.textContent;
+  placeImageInput.value = placeImageInput.textContent;
+  popupPlace.classList.add('popup_opened');
+}
+
+function closePopupPlace () {
+  popupPlace.classList.remove('popup_opened');
+}
+
 openFormButton.addEventListener('click', openPopup);
 formEditProfile.addEventListener('submit', formSubmitHandler);
 closeFormButton.addEventListener('click', closePopup);
+openFormPlaceButton.addEventListener('click', openPopupPlace);
+closeFormPlaceButton.addEventListener('click', closePopupPlace);
+
